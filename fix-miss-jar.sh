20d106949e95e692df0b2963a90e73ba18a0c7c3
shijianhang <3076679995@qq.com>
2021-06-04 09:53:16 +0800

    完善引入jar的脚本, 兼容测试环境

--- fix-miss-jar.sh
+++ fix-miss-jar.sh
@@ -12,7 +12,11 @@ cd tmp/
 jar -xvf $WAR
 rm $WAR
 # 复制javax.servlet-api-3.1.0.jar
-cp /home/shi/.m2/repository/javax/servlet/javax.servlet-api/3.1.0/javax.servlet-api-3.1.0.jar WEB-INF/lib/
+if [ ! -d "/home/shi/.m2" ]; then # 本地
+    cp /home/shi/.m2/repository/javax/servlet/javax.servlet-api/3.1.0/javax.servlet-api-3.1.0.jar WEB-INF/lib/
+else # 测试服
+	cp /root/java/javax.servlet-api-3.1.0.jar WEB-INF/lib/
+fi
 # 重新打包
 jar -cvf $WAR *
 mv $WAR ..
