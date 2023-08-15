ce617098b6f864e08f8711c76a703dce6ceb187b
shijianhang <3076679995@qq.com>
2023-08-15 13:52:21 +0800

    完善gradle

---
+++ src/main/kotlin/net/jkcode/jkmvc/example/controller/TestController.java
@@ -0,0 +1,15 @@
+package net.jkcode.jkmvc.example.controller;
+
+import net.jkcode.jkmvc.example.model.UserModel;
+import net.jkcode.jkmvc.http.controller.Controller;
+
+public class TestController extends Controller {
+	/**
+     * 详情页
+     * @param id*:int:0 用户id
+     * @return {@link net.jkcode.jkmvc.example.model.UserModel }
+     */
+    public void detail()
+    {
+    }
+}
